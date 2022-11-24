5c42325c6a943a21be51635046d21c1f2564fa5b
harryalaw <harryalaw@gmail.com>
2022-11-24 17:00:28 +0000

    Add null safety

--- vm/vm.go
+++ vm/vm.go
@@ -199,6 +199,8 @@ func (vm *VM) executeBangOperator() error {
 		return vm.push(False)
 	case False:
 		return vm.push(True)
+	case Null:
+		return vm.push(True)
 	default:
 		return vm.push(False)
 	}
@@ -226,6 +228,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
