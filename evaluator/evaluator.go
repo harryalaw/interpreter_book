d733671e7f7f47e37aa8d31c179779d603418293
harryalaw <harryalaw@gmail.com>
2024-01-28 13:48:46 +0000

    Refactor builtins, continue on 277

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -133,11 +133,12 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 	case *object.Function:
 		extendedEnv := extendFunctionEnv(fn, args)
 		evaluated := Eval(fn.Body, extendedEnv)
-
 		return unwrapReturnValue(evaluated)
-
 	case *object.Builtin:
-		return fn.Fn(args...)
+		if result := fn.Fn(args...); result != nil {
+			return result
+		}
+		return NULL
 	default:
 		return newError("not a function: %s", fn.Type())
 	}
